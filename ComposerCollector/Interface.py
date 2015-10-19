a02b6c6249d8d35698b98b0600e1645f52ac67c7
Tom Zola <tom@aligent.com.au>
2015-10-19 15:46:32 +1030

    fixed multi versions uploading to satis

--- ComposerCollector/Interface.py
+++ ComposerCollector/Interface.py
@@ -1,9 +1,13 @@
 import xml.etree.cElementTree as ET
 from xml.dom import minidom
+import requests
+import ComposerCollector.Program as program
+import json
+
 __author__ = ''
 
 class StatsToXML:
-
+    ET.Element.set
     @staticmethod
     def frequency_outptut(frequencies, use_lists):
         root = ET.Element("root")
@@ -11,9 +15,7 @@ class StatsToXML:
 
         for package_name, freq in frequencies.items():
 
-            package_elem = ET.SubElement(doc, "package", name=package_name, frequency=str(freq))
-
-            freq_elem = ET.SubElement(package_elem, 'frequency')
+            package_elem = ET.SubElement(doc, "package", name=package_name, use_count=str(freq))
             use_elem = ET.SubElement(package_elem, 'used_by')
 
             if package_name in use_lists:
@@ -27,3 +29,77 @@ class StatsToXML:
         with open("composer_stats.xml", "w") as file:
             file.write(xmlstr)
 
+
+class SatisCommunicator:
+
+    def __init__(self, satis_url, repository_manager):
+        self.satis_url = satis_url
+        self.repository_manager = repository_manager
+
+    def update_satis(self):
+        headers = {'content-type': 'application/json'}
+
+        # add_items = self.get_unique_adds()
+
+        add_items = {'new_items':self.generate_upload()}
+        print(add_items)
+
+        req = requests.post("http://localhost:4680/repo_manager.php",
+                             headers=headers, json=add_items)
+        print(req.text)
+
+    def generate_upload(self):
+        repos = self.repository_manager.repositories
+
+        new_items = list()
+
+        for repo in repos:
+            require = {}
+            comp_repos = []
+            if 'require' in repo.master.composer_json:
+                require = repo.master.composer_json['require']
+            if ('extra' in repo.master.composer_json) \
+                    & ('satis-repositories' in repo.master.composer_json['extra']):
+                comp_repos = repo.master.composer_json['extra']['satis-repositories']
+
+            new_items.append({'repos': comp_repos, 'requires': require})
+
+        return new_items
+
+
+    def get_unique_adds(self):
+        repos = rm.repositories
+
+        new_composer_repos = dict()
+
+        new_requires = dict()
+
+        for repo in repos:
+
+            composer_json = repo.master.composer_json
+
+            if 'require' in composer_json:
+                for req in composer_json['require'].items():
+                    if req not in new_requires:
+                        new_requires[req[0]] = req[1]
+
+            if ('extra' in composer_json) & ('satis-repositories' in composer_json['extra']):
+                for composer_repo in composer_json['extra']['satis-repositories']:
+                    if 'package' not in composer_repo:
+                        new_composer_repos[composer_repo['url']+composer_repo['type']] \
+                            = composer_repo
+
+        return {'new_item': {'repos': list(new_composer_repos.values()), 'requires': new_requires}}
+SATIS_URL = 'http://localhost:4680/repo_manager.php'
+
+
+# Progarm run code test
+rm = program.RepositoryManager()
+rm.load_file('example.xml')
+
+stats = program.Stats(rm.repositories)
+
+satis = SatisCommunicator(SATIS_URL, rm)
+satis.update_satis()
+
+StatsToXML.frequency_outptut(stats.package_use_frequency, stats.package_used_by)
--- ComposerCollector/Program.py
+++ ComposerCollector/Program.py
@@ -5,14 +5,13 @@ import shutil
 import xml.etree.ElementTree
 import _thread
 import json
-import networkx as nx
-import ComposerCollector.Interface as Interface
 __author__ = ''
 
 # Facilitates the loading of repository information
 class RepositoryManager:
 
     REPO_DIR = 'repos'
+    COMPOSER_JSON_DIR = 'sadis_composer_json_files'
 
     def __init__(self):
 
@@ -29,9 +28,12 @@ class RepositoryManager:
         if not os.path.isdir(self.REPO_DIR):
             os.mkdir(self.REPO_DIR)
 
+        # Make directory for storing composer json files - to be used in populating satis
+        if not os.path.isdir(self.REPO_DIR):
+            os.mkdir(self.COMPOSER_JSON_DIR)
+
     # Load XML file of repositories
     def load_file(self, file):
-
         # Parse XML file into python structure
         repo_xml = xml.etree.ElementTree.parse(file).getroot()
 
@@ -44,27 +46,46 @@ class RepositoryManager:
 
         # Create repository entry for each repo already in file system
         for repo_name in existing:
-            self.load_existing_repository(repo_name)
+            in_repo = repo_xml.find("./repositories/repository[@name='%s']" % repo_name)
+
+            if not in_repo:
+                comp_path = in_repo.attrib['composer_path']
+                self.load_existing_repository(repo_name, comp_path)
+            else:
+                print(repo_name + ' is not in XML repository file, repository not loaded')
 
         # Load Repository data for any new repositories, preformed on new threads for
         # simultaneous downloading
-        for child in repo_xml:
+        for child in repo_xml.find("repositories"):
             try:
                 # Flag that a repository is currently loading
                 if child.attrib['name'] not in existing:
                     self.repos_loading += 1
-                    _thread.start_new_thread(self.load_new_repository, (child.attrib['name'], child.attrib['url'],))
+
+                    # Name and URL required, will try without a branch
+                    _thread.start_new_thread(self.load_new_repository, (child.attrib['name'],
+                                                                        child.attrib['url'],
+                                                                        child.attrib['branch'] or '',
+                                                                        child.attrib['composer_path'] or '',))
             except _thread.error:
                 print('Error loading ', child.attrib['name'])
                 self.repos_loading -= 1
 
-    def load_existing_repository(self, name):
+        # Wait for repositories to finish downloading
+        while self.repos_loading > 0:
+            pass
 
+    def load_existing_repository(self, name, comp_path):
+      #  subprocess.Popen(['git', 'config'], cwd='repos/%s/' % name, shell=True)
         repo = git.Repo(os.path.join('repos', name))
-        repo.remote().pull()
-        self.create_repository(name, os.path.join('repos', name))
+        repo.remote().fetch()
+        try:
+            repo.remote().pull()
+        except:
+            print("could not update", name)
+        self.create_repository(name, os.path.join('repos', name), comp_path)
 
-    def load_new_repository(self, name, url):
+    def load_new_repository(self, name, url, branch, comp_path):
 
         # Repositories stored in temp while being created
         complete_path = os.path.join(self.REPO_DIR, name)
@@ -81,7 +102,12 @@ class RepositoryManager:
         print('fetching', name)
         origin.fetch()
         print('fetched', name)
-        origin.pull(origin.refs[0].remote_head)
+
+        if branch and branch != '':
+            print(branch)
+            origin.pull(branch)
+        else:
+            origin.pull(origin.refs[0].remote_head)
 
         print(origin.refs)
 
@@ -90,16 +116,18 @@ class RepositoryManager:
         print(name, "loaded")
 
         self.lock.acquire()
-        self.create_repository(name, complete_path)
+        self.create_repository(name, complete_path, comp_path)
         self.repos_loading -= 1
         self.lock.release()
 
     # Creates a representation of the needed components of a python repository
-    def create_repository(self, name, repo_path):
+    def create_repository(self, name, repo_path, composer_path=''):
 
-        composer_lock = self.load_json_file(repo_path, 'composer.lock')
+        complete_path = os.path.join(repo_path, composer_path)
 
-        composer_json = self.load_json_file(repo_path, 'composer.json')
+        composer_lock = self.load_json_file(complete_path, 'composer.lock')
+
+        composer_json = self.load_json_file(complete_path, 'composer.json')
 
         master = dm.Branch(composer_json, composer_lock)
 
@@ -144,32 +172,3 @@ class Stats:
                         else:
                             self.package_use_frequency[module['name']] += 1
 
-
-class GraphVisualisation:
-
-    def __init__(self, repositories):
-        self.graph = nx.Graph()
-
-        for repo in repositories:
-            if repo.master:
-                if repo.master.composer_lock:
-                    for module in repo.master.composer_lock['packages']:
-                        self.graph.add_edge(repo, module['name'])
-        #nx.draw(self.graph)
-
-test = RepositoryManager()
-test.load_file('example.xml')
-
-# Wait for repositories to finish downloading
-while test.repos_loading > 0:
-    pass
-
-
-stats = Stats(test.repositories)
-graph = GraphVisualisation(test.repositories)
-
-print()
-print(stats.package_use_frequency)
-print(stats.package_used_by)
-
-Interface.StatsToXML.frequency_outptut(stats.package_use_frequency, stats.package_used_by)
